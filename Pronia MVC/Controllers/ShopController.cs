d8630e8718b05bb66eaa8701c836469e4d909ab4
Tagforaz <fordeveloper97@gmail.com>
2025-11-11 19:10:06 +0400

    Initial commit

---
+++ Pronia MVC/Controllers/ShopController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pronia_MVC.Controllers
+{
+    public class ShopController : Controller
+    {
+        [Route("shop")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
